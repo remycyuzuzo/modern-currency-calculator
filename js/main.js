6bb1e48d9ff9aa4ce3a9d227a5bb3f165170f274
Cyuzuzo Jean Remy <remycyuzuzo@gmail.com>
2023-05-02 01:56:48 +0300

    Added a server call

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { env } from "../config";
+
 const currencies = [
     { currency: "USD", rate: 0.7 },
     { currency: "RWF", rate: 0.001 },
@@ -22,9 +24,32 @@ currencySwitchers.forEach((select) => {
     });
 });
 
-function convertCurrency() {
+async function convertCurrency() {
     const inputAmount = amountInputEl.value;
     const inputCurrency = inputCurrencySelector.value;
     const convertTo = outputCurrencySelector.value
+
+    const options = {
+        method: 'GET',
+        url: 'https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest',
+        params: {
+            from: inputCurrency,
+            to: convertTo
+        },
+        headers: {
+            'X-RapidAPI-Key': env.currency_api_key,
+            'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+        }
+    };
+    try {
+        await axios.request(options)
+    } catch (error) {
+        console.warn(error);
+    }
+
 }
 
+document.querySelector("form").addEventListener('submit', e => {
+    e.preventDefault()
+    convertCurrency()
+});
